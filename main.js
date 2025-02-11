144b8036189c9e374ef99f62a9c0f12ea7624058
Zen-nojen <jiyoyo1234@naver.com>
2025-02-11 13:25:14 +0900

    get() 메소드를 추가했습니다.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Task from "./task.js";
-import { DATABASE_URL } from "./env.js";
-
-export const PORT = 3000;
+import { DATABASE_URL, PORT } from "./env.js";
 
 const app = express();
 app.use(express.json());
@@ -12,10 +10,26 @@ await mongoose.connect(DATABASE_URL);
 
 app.post("/tasks", async (req, res) => {
   const data = req.body;
-  console.log(data);
   const newTask = await Task.create(data);
-  console.log(newTask);
   res.status(201).send(newTask);
+});
+
+app.get("/tasks", async (req, res) => {
+  //sort, count
+  const count = Number(req.query.count) || 0;
+  const sortOption =
+    req.query.sort === "oldest" ? ["createdAt", "asc"] : ["createdAt", "desc"];
+  const tasks = await Task.find().limit(count).sort([sortOption]);
+  res.send(tasks);
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
