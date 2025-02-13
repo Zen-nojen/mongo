a6c1151ee0948b3d33f29d569e44dd6b4aa7ddff
Zen-nojen <jiyoyo1234@naver.com>
2025-02-13 10:13:16 +0900

    cors 작업을 완료했습니다.

--- env.js
+++
@@ -1,2 +0,0 @@
-export const DATABASE_URL =
-  "mongodb+srv://awesome37210:qpwoeiru1!@cluster0.2az1b.mongodb.net/todo-api?retryWrites=true&w=majority&appName=Cluster0";
--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import Task from "./task.js";
-// import { DATABASE_URL, PORT } from "./env.js";
 
 dotenv.config();
-
 const app = express();
+
+const corsOptions = {
+  origin: "https://todo.com",
+};
+app.use(cors());
 app.use(express.json());
 
 await mongoose.connect(process.env.DATABASE_URL);
